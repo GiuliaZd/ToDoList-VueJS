59f6330f0ba1be275c52066f47a4e0372928e8c0
GiuliaZd <yuliya.zd@gmail.com>
2024-01-26 12:05:30 +0200

    todo item was added

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">To-Do Item</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoItem",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
